e6a69aaf9b764802506caf95650b1be3c2a5e320
zaidbinusama <zaidbinusama01@gmail.com>
2024-03-03 15:35:43 +0500

    modified:   noteyApp/__pycache__/views.cpython-311.pyc 	modified:   noteyApp/views.py

--- noteyApp/views.py
+++ noteyApp/views.py
@@ -5,6 +5,9 @@ from rest_framework.decorators import api_view
 from .models import QuestionAnswerTable, ContextTable, TopicTable, LeaderBoardTable, UserQuizTable
 from django.contrib.auth.models import User
 from .serializer import QuestionAnswerTableSerializer, ContextTableSerializer, TopicTableSerializer, LeaderBoardTableSerializer, UserQuizTableSerializer, UserSerializer
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -39,4 +42,33 @@ def createQuiz(request, num_questions=5):
     userQuiz.save()
     serializer = UserQuizTableSerializer(userQuiz)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  # Replace 'dashboard' with the URL name of your dashboard view
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            login(request, user)
+            
+            return Response("Login Successful")
+        else:
+            return Response("Login Failed")
+
+
 
